ee8a2f4337059b8bf1b807ce7c4f0754c79e88c2
n--sekiya <n_sekiya@atlas.jp>
2017-06-25 23:25:06 +0900

    サンプルを追加

--- src/startbook/list3/list3_3.kt
+++ src/startbook/list3/list3_3.kt
@@ -1,2 +1,8 @@
 package startbook
 
+class Rational(val numerator: Int, val denominator: Int) {
+	init {
+		require(denominator != 0, {"denominator must not be null"})
+	}
+	override fun toString(): String = "${numerator}/${denominator}}"
+}
--- src/startbook/list3/list3_4.kt
+++ src/startbook/list3/list3_4.kt
@@ -1,2 +1,15 @@
 package startbook.list3
 
+class Rational(n: Int, d:Int) {
+	init{
+		require(d != 0, {"denominator must not be null"})
+	}
+	private val g = gcd(Math.abs(n), Math.abs(d))
+	val numerator: Int = n / g
+	val denominator: Int = d / g
+	
+	override fun toString(): String = "${numerator}/${denominator}"
+	tailrec private fun gcd(a:Int, b: Int): Int = 
+	if (b == 0) a
+	else gcd(b, a % b)
+}
