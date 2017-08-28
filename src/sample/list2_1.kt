0cf1a95c87a881a8c311f278d70800fb07972f7a
n--sekiya <n_sekiya@atlas.jp>
2017-08-28 09:56:00 +0900

    サンプルプログラムを追加

--- src/sample/list2_1.kt
+++
@@ -1,5 +0,0 @@
-//fun main(args: Array<String>) {
-//	var text: String? = "Hello, World!"
-//	println(text.length)
-//}
-//
---
+++ src/sample/nullsafety3.kt
@@ -0,0 +1,6 @@
+fun main(args: Array<String>) {
+	var text: String? = "Hello, World!"
+	if (text != null) {
+	println(text.length)
+	}
+}
